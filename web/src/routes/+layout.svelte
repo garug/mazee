1aa4d8cdaf58ab826c61d31b847a634177c166ea
allef (garug) <allefff@gmail.com>
2025-08-02 16:26:57 -0300

    adjustments

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -19,10 +19,11 @@
     {@render children()}
 </div>
 
-<style>
+<style lang="postcss">
     .wrapper {
         height: 100dvh;
         width: 100dvw;
+        position: relative;
         justify-content: center;
         align-items: center;
         display: flex;
@@ -33,5 +34,31 @@
         font-family: "Jersey 25", sans-serif;
         color: #cfc6b8;
         font-size: 20px;
+    }
+
+    :global(.button) {
+        display: inline-block;
+        cursor: pointer;
+        background-color: #7A444A;
+        color: #cfc6b8;
+        text-decoration: none;
+        padding: 0.65em 0.5em;
+        text-shadow: 2px 2px 0 #472d3c,1px 1px 0 #472d3c;
+        font-size: 36px;
+        transition: all linear .1s;
+        clip-path: polygon(
+            8px 0,
+            calc(100% - 8px) 0,
+            100% 8px,
+            100% calc(100% - 8px),
+            calc(100% - 8px) 100%,
+            8px 100%,
+            0 calc(100% - 8px),
+            0 8px
+        );
+
+        &:hover {
+            transform: scale(1.05);
+        }
     }
 </style>
