bc00472d6b3e9c62a938e1cdd9fb3df83b397e00
allef (garug) <allefff@gmail.com>
2025-08-02 23:06:36 -0300

    test with more properties

--- web/src/routes/daily/+page.ts
+++ web/src/routes/daily/+page.ts
@@ -4,9 +4,9 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad = async ({ fetch }) => {
     const response = await fetch("/api/maze");
 
-    const { token, error: e } = await response.json();
+    const { token, error: e, ...rest } = await response.json();
 
-    console.log({token})
+    console.log({token, e, rest})
 
     if (e) {
         error(response.status, e);
