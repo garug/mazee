99ec06d3a25d83df4dccb126d0106c3d61c40358
allef (garug) <allefff@gmail.com>
2025-08-02 22:42:46 -0300

    test

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,48 +1,48 @@
-import { PUBLIC_DOMAIN, PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL, PUBLIC_URL } from "$env/static/public";
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import type { LayoutLoad } from "./$types";
 import {
     createBrowserClient,
     createServerClient,
-    isBrowser
+    isBrowser,
 } from "@supabase/ssr";
 
 function supabaseBrowser() {
     return createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
         global: {
-            fetch
-        }
+            fetch,
+        },
     });
 }
 
-function supabaseServer(cookies: { name: string; value: string }[]) {
+function supabaseServer(cookies: { name: string; value: string }[], url: URL) {
     return createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
         global: {
-            fetch
+            fetch,
         },
         cookies: {
             getAll() {
                 return cookies;
-            }
+            },
         },
         cookieOptions: {
-            name: 'supabase-session',
-            domain: PUBLIC_DOMAIN || PUBLIC_URL,
+            name: "supabase-session",
+            domain: url.hostname,
             path: "/",
             sameSite: "lax",
             maxAge: 60 * 60 * 24 * 7,
-        }
+        },
     });
 }
 
-export const load: LayoutLoad = async ({ fetch, data, depends }) => {
+export const load: LayoutLoad = async ({ url, data, depends }) => {
     depends("supabase:auth");
 
     const supabase = isBrowser()
         ? supabaseBrowser()
-        : supabaseServer(data.cookies);
+        : supabaseServer(data.cookies, url);
 
     const {
-        data: { session }
+        data: { session },
     } = await supabase.auth.getSession();
 
     return { supabase, session };
--- web/src/routes/+page.server.ts
+++ web/src/routes/+page.server.ts
@@ -1,19 +1,18 @@
 import type { Actions } from "./$types";
-import { PUBLIC_URL } from "$env/static/public";
 import { redirect } from "@sveltejs/kit";
 
 export const actions = {
-    default: async ({ request, locals: { supabase } }) => {
+    default: async ({ request, locals: { supabase }, url }) => {
         const data = await request.formData();
         const email = data.get("email") as string;
 
         await supabase.auth.signInWithOtp({
             email,
             options: {
-                emailRedirectTo: PUBLIC_URL,
-            }
+                emailRedirectTo: url.origin,
+            },
         });
 
         return redirect(303, "/check-email");
-    }
+    },
 } satisfies Actions;
